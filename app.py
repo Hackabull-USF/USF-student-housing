65cab3c60ea53e5184ca821f9cd00a92d69f9060
Siwon-Kim <siwonkim1108@gmail.com>
2023-03-24 14:28:53 -0400

    bring scrapped data

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+import requests, json
+from bs4 import BeautifulSoup
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route("/apt", methods=["GET"])
+def apt_post():
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get('https://www.apartments.com/off-campus-housing/fl/tampa/university-of-south-florida-at-tampa-tampa-campus/',headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    trs = soup.select('#placardContainer > ul > li')
+    apt_info = []
+    for tr in trs:
+        apt = tr.select_one('article > header > div.property-information > a > div.property-title > span')
+        if apt is not None:
+            apt_name = apt.text
+            apt_address = tr.select_one('article > header > div.property-information > a > div.property-address.js-url').text
+            apt_price = tr.select_one('article > section > div > div.property-info > div > div.top-level-info > a > p.property-pricing').text
+            apt_size = tr.select_one('article > section > div > div.property-info > div > div.top-level-info > a > p.property-beds').text
+            info = {
+                'name': apt_name,
+                'address': apt_address,
+                'price': apt_price,
+                'size': apt_size
+            }
+            apt_info.append(info)
+    return jsonify({'apt_info': apt_info})
+
+if __name__ == '__main__':
+	app.run('0.0.0.0', port=5000, debug=True)
+            
