cff32cbfa8ba84f4486e55e7b48614e035728979
destor306 <84240002+destor306@users.noreply.github.com>
2023-03-26 13:22:06 -0400

    marker on apartment on Map

--- frontend/src/components/Maps.js
+++ frontend/src/components/Maps.js
@@ -1,44 +1,83 @@
 import { useMemo, useState, useEffect } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import Geocode from "react-geocode";
+import {GoogleMap, useLoadScript, MarkerF} from "@react-google-maps/api";
 
-export default function Map({position}) {
-  const addr = position["position"]["position"]
+var MarkerLocation
 
-  // const [address, setAddress] = useState(addr);
+export default function Map(position){
+    const { isLoaded } = useLoadScript({
+    googleMapsApiKey: 
+    process.env.REACT_APP_GOOGLE_MAPS_API_KEY,});
 
-  // useEffect(() => {
-  //   Geocode.fromAddress(address)
-  //     .then((response) => {
-  //       const { lat, lng } = response.results[0].geometry.location;
-  //       setAddress(`Latitude: ${lat} Longitude: ${lng}`);
-  //       console.log(lat, lng);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }, []);
 
-	const { isLoaded } = useLoadScript({
-		googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-	});
+    const [center, setCenter] = useState(null);
+    let address_apt = JSON.stringify(position.position.position);
+    //console.log("Map address: ", typeof(address_apt));
+    const addressObj = JSON.parse(address_apt);
+    const full_address = addressObj.position;
+    //console.log(typeof(full_address));
+    const address_parts = full_address.split(",");
+    const streetAddress = address_parts[0].trim();
+    const city = address_parts[1].trim();
+    const state = address_parts[2].trim().slice(0,2);
+    console.log(streetAddress, city , state);
+    const address = `${streetAddress},${city}, ${state}, USA`;
+    console.log("Map address: ", address);
+    useEffect(() => {
+      if(isLoaded && address){
+        
+       
+        // console.log("Map address: ", address);
 
-	if (!isLoaded) return <div>Loading...</div>;
-	return (
-    <Render_Map position={{ lat: -34.397, lng: 150.644 }} />
-  );
+        const geocoder = new window.google.maps.Geocoder();
+        
+        geocoder.geocode(
+          {
+            'address' : address,
+            componentRestrictions:{
+              country: 'US'
+            }
+          },
+        (results, status) =>{
+          if (status === "OK") {
+            setCenter(
+              {
+                lat: results[0].geometry.location.lat(),
+                lng:  results[0].geometry.location.lat()
+              }
+            )
+            MarkerLocation = results[0].geometry.location;
+            console.log(MarkerLocation);
+
+          } else {
+            console.log("Geocode was not successful for the following reason: " + status);
+          }
+          console.log(center);
+        })
+      }
+    })
+    
+   
+    
+    if(!isLoaded) return <div>Loading...</div>
+
+
+  return <GoogleMap  zoom={13} center={MarkerLocation}  mapContainerClassName="map-container">
+    {center && <MarkerF position={MarkerLocation}/>}
+  </GoogleMap>;
 }
 
-function Render_Map({ position }) {
-	const center = useMemo(() => position, []);
+function Render_Map({position}){
+  console.log(position);
+  
+  const center = useMemo(()=> ({lat:28.058892439165348, lng: -82.41362859699025}),[]);
 
-	return (
-		<GoogleMap
-			zoom={10}
-			center={center}
-			mapContainerClassName="map-container"
-		>
-			<Marker position={center} />
-		</GoogleMap>
-	);
+
+  return <GoogleMap 
+  zoom={3} 
+  center ={center} 
+  mapContainerClassName="map-container">
+    <MarkerF  position ={center}/>
+    
+  </GoogleMap>
+  
 }
